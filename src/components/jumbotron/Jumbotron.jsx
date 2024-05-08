f393b76fba36b072e952c535898d2defb6ea7966
hasanarpat <black_the_dragon@hotmail.com>
2024-05-08 21:06:43 +0300

    fix jumbotron height on larger screens and position it center

--- src/components/jumbotron/Jumbotron.jsx
+++ src/components/jumbotron/Jumbotron.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const Jumbotron = () => {
   return (
-    <div className='aspect-video relative rounded-lg mb-8'>
+    <div className='aspect-video max-h-[720px] m-auto relative rounded-lg mb-8'>
       <Image
         alt='jumbo photo'
         src='https://images.unsplash.com/photo-1592780828756-c418d71faa1f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
