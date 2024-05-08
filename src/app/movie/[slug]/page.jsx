4203f1256e517c9746b32f706b434e3d693ea8e8
hasanarpat <black_the_dragon@hotmail.com>
2024-05-08 20:56:02 +0300

    finish detail page design

--- src/app/movie/[slug]/page.jsx
+++ src/app/movie/[slug]/page.jsx
@@ -6,7 +6,7 @@ const MovieDetails = ({ params }) => {
 
   return (
     <div className=''>
-      <div className='w-full h-[720px] lg:h-[520px] relative'>
+      <div className='w-full h-[720px] lg:h-[420px] relative'>
         <Image
           alt='movie'
           src='https://images.unsplash.com/photo-1714891203404-b25f32706e0a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
@@ -14,7 +14,7 @@ const MovieDetails = ({ params }) => {
           className='object-cover opacity-50'
         />
         <div className='absolute z-10 w-full h-full'>
-          <div className='w-full h-full p-4 px-8 flex flex-col lg:flex-row items-center lg:items-stretch gap-4 lg:gap-24'>
+          <div className='w-full h-full p-4 px-8 flex flex-col lg:flex-row items-center lg:items-stretch lg:max-w-max lg:m-auto gap-4 lg:gap-24'>
             <div className='relative w-64 lg:aspect-[9/16] h-full'>
               <Image
                 alt='movie'
@@ -23,7 +23,7 @@ const MovieDetails = ({ params }) => {
                 className='object-cover'
               />
             </div>
-            <div className='flex flex-col gap-2 max-w-2xl'>
+            <div className='flex flex-col gap-2 max-w-2xl justify-center'>
               <h2 className='text-2xl xl:text-4xl font-semibold'>
                 The Lord Of The Rings
               </h2>
@@ -44,7 +44,7 @@ const MovieDetails = ({ params }) => {
           </div>
         </div>
       </div>
-      <div className='flex flex-col gap-2 p-4 px-8'>
+      <div className='flex flex-col gap-2 lg:gap-6 p-4 px-8 max-w-max m-auto'>
         <div className='flex flex-col md:flex-row items-start gap-2'>
           <span>Categories:</span>
           <ul className='flex flex-wrap items-center gap-2'>
@@ -107,6 +107,14 @@ const MovieDetails = ({ params }) => {
             </li>
           </ul>
         </div>
+      </div>
+      <div className='flex justify-center mb-6'>
+        <video
+          controls
+          autoPlay
+          muted
+          src='https://imdb-video.media-imdb.com/vi1951123225/1434659607842-pgv4ql-1707582799965.mp4?Expires=1715276845&Signature=lh1XzBrXZjPRWGIQ96UYtU2PJ-e5vSYS52h-nHWODFwCfnrjsLLpNLAgfrNOLNS9UCaRJ1RvQWAUD13V9DSGdZO~Q3GbcZDKPnk27ZcLAOz3~XP6pxab3X3z3H-OcCdHtHK0ruvXJPuEZLsi-DOD22byuPKruHwfxKmS6lAb7KCgdJgtSQZSHWsjdlyK1Emui~yxJ5Id~5B-x~stQBGEgA7uUlgApvOOXJN-i~cYBmINAlIh8wa0FlorJhF1bk44NNleW7uPCWJAkbDr9xySYeNegbP65glkU031fVgS3kOivoOHIoWdm5P7lZtfDK~vteLQiVdjNl-MM-As5vjSQQ__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA'
+        ></video>
       </div>
     </div>
   );
